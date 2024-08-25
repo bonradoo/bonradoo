92d664b81581af19abf746c3036c1891850a0da2
bonradoo <264061@student.pwr.edu.pl>
2024-08-25 20:45:29 +0200

    Portfolio update

--- portfolio/blueprints/home/home.py
+++ portfolio/blueprints/home/home.py
@@ -2,6 +2,29 @@ from flask import Blueprint, render_template
 
 home_bp = Blueprint('home', __name__, static_folder="static", template_folder="templates")
 
+PROJECTS = [
+        {
+            "title": "CARGO-PRO",
+            "details": "Project Details: A logistics management system.",
+            "image_url": "static/cargo-pro.png",
+            "url": "https://cargo-pro.pl"
+        },
+        {
+            "title": "Konrad Bik",
+            "details": "Portfolio: Cybersecurity specialist and sport enthusiast.",
+            "image_url": "static/konrad-bik.jpg",
+            "logo": "static/logo_yellow.png",
+            "url": "https://konradbik.com"
+        },
+        {
+            "title": "DEEPFAKEDETECTOR",
+            "details": "Project Details: AI-powered deepfake detection tool.",
+            "image_url": "static/deepfakedetector.png",
+            "url": "https://image.deepfakedetector.pl",
+            "hero_image": "static/deepfakedetector.png"
+        }
+    ]
+
 @home_bp.route('/')
 def homepage():
-    return render_template('home.html')
+    return render_template('home.html', projects=PROJECTS)
